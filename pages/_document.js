32b6476f5ad263b0dc1165eaf4d55d7f8ff7c07d
Gerard Prades <gerardprades2@gmail.com>
2024-05-23 21:42:58 +0200

    recuperamos tagmana

--- pages/_document.js
+++ pages/_document.js
@@ -1,18 +1,12 @@
-import { Html, Head, Main, NextScript } from 'next/document';
-import Script from 'next/script'; // Importa el componente Script
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="es">
-      <Head>
-        {/* Otros elementos en el <head>, si necesitas */}
-      </Head>
+      <Head>{/* Otros elementos en el <head>, si necesitas */}</Head>
       <body>
         {/* Utiliza el componente Script para manejar la carga de Google Tag Manager */}
-        <Script 
-          strategy="afterInteractive" 
-          id="gtm-script" // Añade un ID único al script
-        >
+        <script strategy="afterInteractive">
           {`
             (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
@@ -20,14 +14,14 @@ export default function Document() {
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
             })(window,document,'script','dataLayer','GTM-WRCK65N7');
           `}
-        </Script>
+        </script>
         {/* Noscript tag para Google Tag Manager */}
         <noscript>
           <iframe
             src="https://www.googletagmanager.com/ns.html?id=GTM-WRCK65N7"
             height="0"
             width="0"
-            style={{ display: 'none', visibility: 'hidden' }}
+            style={{ display: "none", visibility: "hidden" }}
           ></iframe>
         </noscript>
         <Main />
